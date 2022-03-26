78ea00ab85e78049f40eccfb6d189461ed1b6fb7
ebo2022 <99616031+ebo2022@users.noreply.github.com>
2022-03-26 19:48:00 -0400

    Add bonemealing mixin for lilies

--- common/src/main/java/com/teamaurora/bayou_blues/core/mixin/BoneMealItemMixin.java
+++ common/src/main/java/com/teamaurora/bayou_blues/core/mixin/BoneMealItemMixin.java
@@ -1,15 +1,19 @@
 package com.teamaurora.bayou_blues.core.mixin;
 
+import com.teamaurora.bayou_blues.common.block.LilyFlowerBlock;
 import com.teamaurora.bayou_blues.core.registry.BayouBluesBlocks;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.BoneMealItem;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.DoublePlantBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
+import net.minecraft.world.level.material.Fluids;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,6 +25,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
  */
 @Mixin(BoneMealItem.class)
 public class BoneMealItemMixin {
+    private static boolean checkAdjacentForSolid(Level level, BlockPos pos) {
+        for (int i = 0; i < 4; i++) {
+            Direction dir = Direction.from2DDataValue(i);
+            BlockPos poffset = pos.offset(dir.getNormal());
+
+            if (level.getFluidState(poffset).getType() != Fluids.WATER) {
+                return true;
+            }
+        }
+        return false;
+    }
     @Inject(method = "useOn", at = @At("HEAD"),cancellable = true)
     public void useOn(UseOnContext context, CallbackInfoReturnable<InteractionResult> cir) {
         Level world = context.getLevel();
@@ -29,7 +44,17 @@ public class BoneMealItemMixin {
             boolean success = false;
             int shrinkAmount = 1;
             BlockState state = world.getBlockState(pos);
-            if (state.getBlock() == Blocks.LARGE_FERN) {
+            if (state.getBlock() == Blocks.LILY_PAD) {
+                if (world.getRandom().nextBoolean() || checkAdjacentForSolid(world, pos.below())) {
+                    Block lily = LilyFlowerBlock.getRandomLily(world.getRandom());
+
+                    world.setBlock(pos, lily.defaultBlockState(), 3);
+                    success = true;
+                }
+            } else if (state.getBlock() instanceof LilyFlowerBlock) {
+                Block.popResource(world, pos, state.getBlock().getCloneItemStack(world, pos, state));
+                success = true;
+            } else if (state.getBlock() == Blocks.LARGE_FERN) {
                 if (state.getValue(DoublePlantBlock.HALF) == DoubleBlockHalf.LOWER) {
                     DoublePlantBlock.placeAt(world, BayouBluesBlocks.GIANT_FERN.get().defaultBlockState(), pos, 3);
                 } else {
