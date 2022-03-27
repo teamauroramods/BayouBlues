46e88ebfade8d63ca30153a177ea8aa5b041ee0f
ebo2022 <99616031+ebo2022@users.noreply.github.com>
2022-03-27 13:47:21 -0400

    Add Farmer's Delight compatibility

--- common/src/main/java/com/teamaurora/bayou_blues/core/registry/BayouBluesItems.java
+++ common/src/main/java/com/teamaurora/bayou_blues/core/registry/BayouBluesItems.java
@@ -1,10 +1,9 @@
 package com.teamaurora.bayou_blues.core.registry;
 
-import com.teamaurora.bayou_blues.common.item.AlgaeItem;
 import com.teamaurora.bayou_blues.common.item.DrinkItem;
 import com.teamaurora.bayou_blues.common.item.JamItem;
-import com.teamaurora.bayou_blues.common.item.LilyItem;
 import com.teamaurora.bayou_blues.core.BayouBlues;
+import gg.moonflower.pollen.api.platform.Platform;
 import gg.moonflower.pollen.api.registry.PollinatedRegistry;
 import net.minecraft.core.Registry;
 import net.minecraft.world.food.FoodProperties;
@@ -28,14 +27,7 @@ public class BayouBluesItems {
 
     public static final Supplier<Item> HONEY_GLAZED_GOOSEBERRIES = registerItem("honey_glazed_gooseberries", () -> new Item(new Item.Properties().food(Foods.HONEY_GLAZED_GOOSEBERRIES).tab(CreativeModeTab.TAB_FOOD)));
     public static final Supplier<Item> GOOSEBERRY_JAM = registerItem("gooseberry_jam", () -> new JamItem(new Item.Properties().food(Foods.GOOSEBERRY_JAM).stacksTo(16).tab(CreativeModeTab.TAB_FOOD)));
-    public static final Supplier<Item> GOOSEBERRY_JAM_COOKIE = registerItem("gooseberry_jam_cookie", () -> new Item(new Item.Properties().food(Foods.GOOSEBERRY_JAM_COOKIE).tab(CreativeModeTab.TAB_FOOD)));
-
-
-    /* Lilies */
-
-
-    /* Other */
-
+    public static final Supplier<Item> GOOSEBERRY_JAM_COOKIE = registerItem("gooseberry_jam_cookie", () -> new Item(new Item.Properties().food(Platform.isModLoaded("farmersdelight") ? Foods.GOOSEBERRY_JAM_COOKIE_FAST : Foods.GOOSEBERRY_JAM_COOKIE).tab(CreativeModeTab.TAB_FOOD)));
 
     public static final class Foods {
         public static final FoodProperties GOOSEBERRIES = new FoodProperties.Builder().nutrition(2).saturationMod(0.2F).build();
@@ -45,6 +37,7 @@ public class BayouBluesItems {
         public static final FoodProperties HONEY_GLAZED_GOOSEBERRIES = new FoodProperties.Builder().nutrition(7).saturationMod(0.3F).build();
         public static final FoodProperties GOOSEBERRY_JAM = new FoodProperties.Builder().nutrition(2).saturationMod(0.25F).build();
         public static final FoodProperties GOOSEBERRY_JAM_COOKIE = new FoodProperties.Builder().nutrition(2).saturationMod(0.3F).build();
+        public static final FoodProperties GOOSEBERRY_JAM_COOKIE_FAST = new FoodProperties.Builder().nutrition(2).saturationMod(0.3F).fast().build();
     }
 
     public static Supplier<Item> registerItem(String id, Supplier<Item> item) {
